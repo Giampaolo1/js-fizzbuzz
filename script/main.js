6032d3bcef5185d8602b7673c8b36775fffae83b
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-24 19:08:33 +0200

    funziona

--- script/main.js
+++ script/main.js
@@ -21,10 +21,12 @@
 // Scrivi un programma che stampi i numeri da 1 a 100,
 
 for (var i = 1; i <= 100; i++) {
-  if (i % 3 == 0) {
-    console.log("fizz");
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
 } else if (i % 5 == 0) {
-  console.log("buzz");
+  console.log("Buzz");
+} else if (i % 3 == 0) {
+  console.log("Fizz");
 } else {
   console.log(i);
 }
