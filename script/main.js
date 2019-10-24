d7cdee1b3f70f5029efc23b35fb4e56d3fc4fc96
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-24 18:41:31 +0200

    annidare if might be a solution?

--- script/main.js
+++ script/main.js
@@ -21,14 +21,22 @@
 // Scrivi un programma che stampi i numeri da 1 a 100,
 
 for (var i = 1; i <= 100; i++) {
+  if (i % 3 == 0) {
+    console.log("fizz");
+} else if (i % 5 == 0) {
+  console.log("buzz");
+} else {
   console.log(i);
 }
+}
+// annidare if?
 
 // ma per i multipli di 3 stampi “Fizz” al posto del numero
-i = false
-  if (i % 3 === 0) {
-  i = true;
-}
-if (i === true) {
-  console.log([i], "fizz");
-}
+// e per i multipli di 5 stampi Buzz.
+// else, stamp the number
+
+
+
+// if (i === true) {
+//   console.log(i, "fizz");
+// }
