d2fb32465518059f9b861406d67c538976f0c2ea
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-29 19:51:45 +0100

    a

--- script/main.js
+++ script/main.js
@@ -41,13 +41,130 @@ for (var i = 1; i <= 100; i++) {
 
 // OUTPUT
 // document.getElementById('stamp').innerHTML =  i;
-
-
-
-
-
-
-
 // if (i === true) {
 //   console.log(i, "fizz");
 // }
+
+// IDEA:
+// simo version:
+
+// EX classe Fizz-Buzz
+
+
+//vers base
+/*
+for (var i = 1; i<=100; i++){
+
+  if ( (i % 3 === 0) || (i % 5 === 0)){
+
+    if((i % 3 === 0) && (i % 5 === 0)){
+      console.log("FizzBuzz");
+    } else {
+
+        if(i % 3 === 0){
+          console.log("Fizz");
+        }
+        if (i % 5 === 0){
+          console.log("Buzz")
+        }
+    }
+
+
+  } else{
+    console.log(i);
+  }
+
+
+}
+*/
+
+
+// //vers alt ottimizzata
+// /**/
+// var label;
+//
+// for (var i = 1; i<=100; i++){
+//
+//   label = "";
+//
+//   if ( (i % 3 === 0) || (i % 5 === 0)){
+//
+//         if(i % 3 === 0){
+//           label += "Fizz"
+//         }
+//         if (i % 5 === 0){
+//           label += "Buzz"
+//         }
+//
+//     console.log(label);
+//
+//   } else{
+//     console.log(i);
+//   }
+
+
+// }
+
+
+// //vers alt ottimizzata
+// /**/
+// var label;
+//
+// for (var i = 1; i<=100; i++){
+//
+//   label = "";
+//
+//   if ( (i % 3 === 0) || (i % 5 === 0)){
+//
+//         if(i % 3 === 0){
+//           label += "Fizz"
+//         }
+//         if (i % 5 === 0){
+//           label += "Buzz"
+//         }
+//
+//     console.log(label);
+//
+//   } else{
+//     console.log(i);
+//   }
+//
+//
+// }
+
+
+
+
+//vers alt ottimizzata + Function
+
+/*
+function fizzBuzz (indx, tm){
+  var label;
+
+  for (var i = indx; i<=tm; i++){
+
+    label = "";
+
+    if ( (i % 3 === 0) || (i % 5 === 0)){
+
+      if(i % 3 === 0){
+        label += "Fizz"
+      }
+      if (i % 5 === 0){
+        label += "Buzz"
+      }
+
+      console.log(label);
+
+    } else{
+      console.log(i);
+    }
+
+  }
+}
+
+
+fizzBuzz (1,30);
+console.log("------------");
+fizzBuzz (15,30);
+*/
